0a8dab7f530c4d320e57f4f78d96a9935ef18102
Ken Seal <kenneth.seal@livingsocial.com>
2014-12-30 10:43:36 -0700

    Write decrypt to stdout

--- bin/decrypt.sh
+++ bin/decrypt.sh
@@ -5,14 +5,14 @@
 usage() {
 cat <<-USAGE
 
-    usage: decrypt.sh [input file] [output file]
+    usage: decrypt.sh [input file]
 
 USAGE
 }
 
-if [[ -z $1 || -z $2 ]]; then
+if [[ -z $1 ]]; then
     usage
     exit 1
 fi
 
-/usr/bin/env openssl enc -cast5-cfb -d -in $1 -out $2
+/usr/bin/env openssl enc -cast5-cfb -d -in $1
