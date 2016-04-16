de442d97ddaef727295d3b9dd7aa2be911f54137
Kenneth Seal <hunzinker@gmail.com>
2016-04-16 12:06:40 -0600

    Remove bridge ip from vm

--- bin/create_vm.sh
+++ bin/create_vm.sh
@@ -22,8 +22,3 @@ if [ $? -ne 0 ]; then
     docker-machine ls
     exit 1
 fi
-
-# Setup bridge ip address
-docker-machine ssh $vm sudo "sed -r \"s/EXTRA_ARGS='(.*?)'/EXTRA_ARGS='\1 --bip 10.100.200.1\/24'/\" /var/lib/boot2docker/profile > tmp && sudo mv tmp /var/lib/boot2docker/profile"
-docker-machine stop $vm
-docker-machine start $vm
